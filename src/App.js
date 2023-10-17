38609766d956aaba609329555ea150b13d2cd0fb
hackersidh <bt19eee017@nituk.ac.in>
2023-10-17 19:54:03 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
+import Login from './Authorisation/Login';
+import Register from './Authorisation/Register';
+import Injury_form from './Injury/Injury_form';
+import Injury_list from './Injury/Injury_list';
+import Home from './Components/Home';
+
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'; 
+
 
 function App() {
   return (
+    <Router>
+       <Navbar/>
     <div className="App">
-      <Navbar/>
-
-      
+      <Routes>
+        <Route path="/" element={Home} />
+        <Route path="/login" element={Login} />
+        <Route path="/register" element={Register} />
+        <Route path ="/injuries" element={Injury_list}/>
+        <Route path ="/injuriesForm" element={Injury_form}/>
+        </Routes>
+        
     </div>
+    </Router>
   );
 }
 
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {nav} from 'react-bootstrap';
 import './Navbar.css';
+import { Link } from 'react-router-dom';
 
 
 const Navbar = () => {
@@ -8,27 +9,37 @@ const Navbar = () => {
     <div >    
         <nav class="navbar navbar-expand-lg " id="main">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
+      
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0 text-white" id="text-color">
-          <li class="nav-item text-white" >
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item ">
-            <a class="nav-link " href="#" >
-              Dropdown
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0 text-white" id="">
+          
+         
+         
+          
+          {/* <li class="nav-item ">
+            <a class="nav-link " href="#" > Dropdown
             </a>
-            
+             */}
+          {/* </li> */}
+          <li class="nav-item text-white" >
+            <a class="nav-link active" aria-current="page" href="#"><Link to={"/"}>Home</Link></a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+          <li class="nav-item text-white" >
+            <a class="nav-link active" aria-current="page" href="#"><Link to={"/login"}>Login</Link></a>
           </li>
+          <li class="nav-item text-white" >
+            <a class="nav-link active" aria-current="page" href="#"><Link to={"/register"}>Register</Link></a>
+          </li>
+          <li class="nav-item text-white" >
+            <a class="nav-link active" aria-current="page" href="#"><Link to={"/injuries"}>Injuries_List</Link></a>
+          </li>
+          <li class="nav-item text-white" >
+            <a class="nav-link active" aria-current="page" href="#"><Link to={"/injuriesForm"}>Injuries_Form</Link></a>
+          </li>
+         
         </ul>
         <form class="d-flex" role="search">
           
